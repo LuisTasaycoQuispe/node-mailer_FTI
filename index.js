31e7dbf0fc3ba0f867b53b45b10f845403bec45f
Luis Tasayco <127450834+LuisTasaycoQuispe@users.noreply.github.com>
2026-04-16 15:48:14 +0000

    correeicon

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const corsOptions = {
   optionsSuccessStatus: 200 
 };
 
-app.use(cors(corsOptions))
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
