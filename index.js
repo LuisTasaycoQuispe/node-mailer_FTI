5fb94d07c3cd5fe0d60b97a00d28592451afb56f
Luis Tasayco <127450834+LuisTasaycoQuispe@users.noreply.github.com>
2026-04-21 18:15:36 +0000

    actualizacion de correo

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/evaluacion', async (req, res) => {
 // marco.paredes@fiestatoursperu.com
             await transporter.sendMail({
                 from: `"Fiesta Tours Peru" <${EMAIL_USER}>`,
-                to: "marco.paredes@fiestatoursperu.com, dw@fiestatoursperu.com",
+                to: "marco.paredes@fiestatoursperu.com, repevacusco@gmail.com",
                 subject: `Evaluacion Viaje - ${nombre}`,
                   html: `
                     <div style="background:#f4f6f5; padding:20px 0;">
